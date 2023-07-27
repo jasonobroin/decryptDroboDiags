978587c810254db54a739fbdf52c91c6790a2569
Jason O'Broin <jasonob@meraki.com>
2023-07-26 23:15:48 -0700

    Remove -mod=readonly
    No longer needed as using go.mod and NOT using vendor directory

--- buildall.sh
+++ buildall.sh
@@ -1,10 +1,10 @@
 echo Building Linux/x86
-env GOOS=linux GOARCH=amd64 go build -mod=readonly -ldflags "-s -w"
+env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w"
 mv decryptDiags decryptDiags-lx
 echo Building Windows
-env GOOS=windows GOARCH=386 go build -mod=readonly -ldflags "-s -w"
+env GOOS=windows GOARCH=386 go build -ldflags "-s -w"
 echo Building Mac
-env GOOS=darwin GOARCH=amd64 go build -mod=readonly -ldflags "-s -w"
+env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w"
 
 # Build docker image
 #docker build -t decryptdiags .
